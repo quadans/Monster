03e630fd31547b182d3368072b19cea0612e41d5
Dennis K <golden_talon@hotmail.de>
2019-02-14 18:14:12 +0100

    erster Vorschritt

--- monstergame/monstergame/main.cpp
+++ monstergame/monstergame/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+
+struct monster {
+
+};
+
+
 int main()
 {
+
+	enum class Monstertypen
+	{
+		OGRE,
+		DRAGON,
+		ORC,
+		GIANT_SPIDER,
+		SLIME,
+	};
+
+	Monstertypen Auswahl = Monstertypen::OGRE;
+
+	if (Auswahl == Monstertypen::OGRE)
+		std::cout << "Du hast Ogre ausgewaehlt!";
+
 	return 0;
 }
